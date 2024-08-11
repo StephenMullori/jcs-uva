e46c453964282a409d793b4a6cc4cc45b8269700
StephenMullori <70962083+StephenMullori@users.noreply.github.com>
2024-08-10 21:10:34 -0400

    removed custom cms

--- src/routes/authenticated/console/new-article/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import NewArticleForm from '$lib/forms/NewArticle/NewArticleForm.svelte';
-	import type { PageData } from './$types';
-	const { data }: { data: PageData } = $props();
-</script>
-
-<NewArticleForm data={data.form} />
--- src/routes/testing/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import QuillEditor from '$lib/components/QuillEditor/QuillEditor.svelte';
-	import type { PageData } from './$types';
-
-	let { data }: { data: PageData } = $props();
-</script>
