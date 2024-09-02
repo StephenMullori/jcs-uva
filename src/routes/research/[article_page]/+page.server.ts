dd83268e5fae7fb0819ac99a9205697d95ed19cf
StephenMullori <70962083+StephenMullori@users.noreply.github.com>
2024-09-01 22:55:56 -0400

    removed a print statement

--- src/routes/research/[article_page]/+page.server.ts
+++ src/routes/research/[article_page]/+page.server.ts
@@ -9,6 +9,5 @@ export const load = (async (event) => {
 	if (article.length === 0) {
 		throw error(404, 'Page not found');
 	}
-	console.log(article[0]);
 	return { article: article[0] };
 }) satisfies PageServerLoad;
